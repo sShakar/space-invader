a0918806d1e551c264a59f6e5777499f38eb329c
Shakar Kosar <shakarkosar01@gmail.com>
2024-02-25 15:45:49 +0300

    feat: added image

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+
 # Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill((0, 0, 0))
+    pygame.display.update()
